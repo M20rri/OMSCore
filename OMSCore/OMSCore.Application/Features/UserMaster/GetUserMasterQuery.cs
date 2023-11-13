c5f994e8b70e94b008a95dcffaa05ed47c78409d
M2ri <SergiGullam@gmail.com>
2023-11-13 15:04:53 +0200

    Apply getAll

---
+++ OMSCore/OMSCore.Application/Features/UserMaster/GetUserMasterQuery.cs
@@ -0,0 +1,25 @@
+﻿using MediatR;
+using OMSCore.Application.Interfaces.Repositories;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace OMSCore.Application.Features.UserMaster
+{
+    public record GetUserMasterQuery() : IRequest<IEnumerable<Domain.Entities.UserMaster>>;
+
+    public sealed class GetUserMasterHandler : IRequestHandler<GetUserMasterQuery, IEnumerable<Domain.Entities.UserMaster>>
+    {
+        private readonly IUserMasterRepositoryAsync _userMasterRepositoryAsync;
+
+        public GetUserMasterHandler(IUserMasterRepositoryAsync userMasterRepositoryAsync)
+        {
+            _userMasterRepositoryAsync = userMasterRepositoryAsync;
+        }
+
+        public async Task<IEnumerable<Domain.Entities.UserMaster>> Handle(GetUserMasterQuery request, CancellationToken cancellationToken)
+        {
+            return await _userMasterRepositoryAsync.GetAllAsync();
+        }
+    }
+}
---
+++ OMSCore/OMSCore.WebApi/Controllers/v1/UserMasterController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using OMSCore.Application.Features.UserMaster;
+using System.Net;
+using System.Threading.Tasks;
+
+namespace OMSCore.WebApi.Controllers.v1
+{
+    [Authorize]
+    [Route("api/UserMaster")]
+    [ApiVersion("1.0")]
+    public class UserMasterController : BaseApiController
+    {
+        [HttpGet, Route("api/get-all")]
+        public async Task<IActionResult> GetAll()
+        {
+            return CustomResult(await Mediator.Send(new GetUserMasterQuery()), HttpStatusCode.OK);
+        }
+    }
+}
